ed0fd8ccfd3bfcb5260f692c36f01ae08a348d23
Xiaoyu Li <xiaoyuli2019@gmail.com>
2020-11-27 20:37:45 -0500

    init project

---
+++ Source/SwordOfMaria/SwordOfMariaGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SwordOfMariaGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SWORDOFMARIA_API ASwordOfMariaGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
